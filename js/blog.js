9f38eab30a555fba57f1fecac1e476bfd4909bdf
Pawel <113646840+pawelwlo@users.noreply.github.com>
2023-12-23 03:23:43 +0100

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,4 @@
-import { db } from "/js/firebase.js";
+import { db } from "/dieta-blog/js/firebase.js";
 import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 
