5b944b4fb1c75b489ecc0b062d8c5778963e25a4
Pawel <113646840+pawelwlo@users.noreply.github.com>
2023-12-23 03:21:58 +0100

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-import { db } from "/js/firebase.js";
+import { db } from "/dieta-blog/js/firebase.js";
 import { doc, getDocs, collection } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const blogSection = document.querySelector('.blog-section');
