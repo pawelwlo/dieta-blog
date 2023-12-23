8aded2fd7d8e12913a26e2aab4f31e8f2b880635
Pawel <113646840+pawelwlo@users.noreply.github.com>
2023-12-23 04:08:32 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,26 +1,23 @@
 
  
+// Import necessary Firebase modules from the locally installed package
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/analytics';
 
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-  import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
- 
- 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyBRkumXWdsx_YER65uUDLyw0YYdRyn-2CE",
-    authDomain: "diet-blog-b8ca5.firebaseapp.com",
-    projectId: "diet-blog-b8ca5",
-    storageBucket: "diet-blog-b8ca5.appspot.com",
-    messagingSenderId: "1086895413120",
-    appId: "1:1086895413120:web:9159ecdebe13c4b29f9365"
-  };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBRkumXWdsx_YER65uUDLyw0YYdRyn-2CE",
+  authDomain: "diet-blog-b8ca5.firebaseapp.com",
+  projectId: "diet-blog-b8ca5",
+  storageBucket: "diet-blog-b8ca5.appspot.com",
+  messagingSenderId: "1086895413120",
+  appId: "1:1086895413120:web:9159ecdebe13c4b29f9365"
+};
 
-  // Use this to initialize the firebase App
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
-  const analytics = getAnalytics(app);
-  
-  export {db};
+// Initialize Firebase using the locally imported firebase object and configuration
+const app = firebase.initializeApp(firebaseConfig);
+const db = app.firestore();
+const analytics = app.analytics();
 
-
+export { db, analytics };
