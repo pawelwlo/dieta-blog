6fae681e5c523ec9d55bbe59569339d1a67a2a53
Pawel <113646840+pawelwlo@users.noreply.github.com>
2023-12-23 02:40:24 +0100

    new commit

--- server.js
+++ server.js
@@ -5,54 +5,61 @@ const fileupload = require('express-fileupload');
 let initial_path = path.join(__dirname, "/");
 
 const app = express();
-app.use(express.static(initial_path));
+
+// Serve static files (including JavaScript) from the initial_path directory
+app.use(express.static(initial_path, { 
+  setHeaders: (res, path, stat) => {
+    if (path.endsWith('.js')) {
+      res.set('Content-Type', 'application/javascript');
+    }
+  }
+}));
+
 app.use(fileupload());
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(initial_path, "/home.html"));
-})
-
-app.get('/editor', (req, res) => {
-    res.sendFile(path.join(initial_path, "/editor.html"));
-})
-
-//upload link
-
-app.post('/uploads', (req, res) => {
-    if (!req.files || !req.files.image) {
-        return res.status(400).json({ error: 'No file uploaded' });
-    }
-
-    const file = req.files.image;
-    const date = new Date();
-    const imagename = date.getDate() + date.getTime() + file.name;
-    const uploadPath = path.join(__dirname, 'uploads', imagename);
-
-    file.mv(uploadPath, (err) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-
-        res.json(`uploads/${imagename}`);
-    });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(initial_path, "/home.html"));
 });
 
-app.get("/:blog", (req,res) => {
-    res.sendFile(path.join(initial_path, "/blog.html"));
-}) 
+app.get('/editor', (req, res) => {
+  res.sendFile(path.join(initial_path, "/editor.html"));
+});
 
-app.get("/:about", (req,res) => {
-    res.sendFile(path.join(initial_path, "/about.html"));
-}) 
+app.post('/uploads', (req, res) => {
+  if (!req.files || !req.files.image) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
 
-app.get("/:contact", (req,res) => {
-    res.sendFile(path.join(initial_path, "/contact.html"));
-}) 
-app.use((req,res) => {
-    res.json("404");
-})
+  const file = req.files.image;
+  const date = new Date();
+  const imagename = date.getDate() + date.getTime() + file.name;
+  const uploadPath = path.join(__dirname, 'uploads', imagename);
 
+  file.mv(uploadPath, (err) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+
+    res.json(`uploads/${imagename}`);
+  });
+});
+
+app.get("/:blog", (req, res) => {
+  res.sendFile(path.join(initial_path, "/blog.html"));
+});
+
+app.get("/:about", (req, res) => {
+  res.sendFile(path.join(initial_path, "/about.html"));
+});
+
+app.get("/:contact", (req, res) => {
+  res.sendFile(path.join(initial_path, "/contact.html"));
+});
+
+app.use((req, res) => {
+  res.json("404");
+});
 
 app.listen("3000", () => {
-    console.log('listening.....');
-})
+  console.log('listening.....');
+});
