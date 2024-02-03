88af5b08937580e513eb8117328d3109debc0dfa
Pawel <113646840+pawelwlo@users.noreply.github.com>
2024-02-03 04:14:41 +0100

    Update editor.js

--- js/editor.js
+++ js/editor.js
@@ -22,7 +22,7 @@ uploadInput.addEventListener('change', () => {
     uploadImage(uploadInput, "image");
 })
 
-console.log("this is location.origin:" location.origin);
+console.log("this is location.origin:" (location.origin));
 
 const uploadImage = (uploadFile, uploadType) => {
     const [file] = uploadFile.files;
