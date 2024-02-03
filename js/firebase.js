419126bd737d93fbf74294c789560d940a18ba6d
Pawel <113646840+pawelwlo@users.noreply.github.com>
2024-02-03 03:51:32 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,23 +1,28 @@
 
  
 
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-  import { getFirestore, doc, getDoc } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js"";
+  import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
  
  
   // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyBRkumXWdsx_YER65uUDLyw0YYdRyn-2CE",
-    authDomain: "diet-blog-b8ca5.firebaseapp.com",
-    databaseURL: "https://diet-blog-b8ca5-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "diet-blog-b8ca5",
-    storageBucket: "diet-blog-b8ca5.appspot.com",
-    messagingSenderId: "1086895413120",
-    appId: "1:1086895413120:web:9159ecdebe13c4b29f9365",
-    measurementId: "G-DJXKKYQTW3"
-  
-  };
+ const firebaseConfig = {
+
+  apiKey: "AIzaSyDKAxo--9ty2t4PRSQG2_MV47K3w0JsKSc",
+
+  authDomain: "new-diet.firebaseapp.com",
+
+  projectId: "new-diet",
+
+  storageBucket: "new-diet.appspot.com",
+
+  messagingSenderId: "555542102605",
+
+  appId: "1:555542102605:web:4665cb8e689f0235951204"
+
+};
+
 
   // Use this to initialize the firebase App
   const app = initializeApp(firebaseConfig);
