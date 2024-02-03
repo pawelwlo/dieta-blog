eda943e6d017b231f78bef171943368afc7ca9cf
Pawel <113646840+pawelwlo@users.noreply.github.com>
2024-02-03 03:22:34 +0100

    Update editor.js

--- js/editor.js
+++ js/editor.js
@@ -1,29 +1,6 @@
 
-// import {db} from "./js/firebase.js"
+import { db } from '../js/firebase.js';
 import { doc, setDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-  import { getFirestore, doc, getDoc } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
- 
- 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyBRkumXWdsx_YER65uUDLyw0YYdRyn-2CE",
-    authDomain: "diet-blog-b8ca5.firebaseapp.com",
-    databaseURL: "https://diet-blog-b8ca5-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "diet-blog-b8ca5",
-    storageBucket: "diet-blog-b8ca5.appspot.com",
-    messagingSenderId: "1086895413120",
-    appId: "1:1086895413120:web:9159ecdebe13c4b29f9365",
-    measurementId: "G-DJXKKYQTW3"
-  
-  };
-
-  // Use this to initialize the firebase App
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
- 
  
 
 const blogTitleField = document.querySelector('.title');
